6fb37399db7cec463c8a79d2a30f5dc0dde0d5ea
sevengoldfinanceira <sevengoldfinanceira@gmail.com>
2026-07-01 21:40:19 -0300

    Atualiza icones das metricas comerciais

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "seven-gold-appointment-conversion-inside-card-v15";
+const CACHE_VERSION = "seven-gold-commercial-metric-icons-v16";
 const STATIC_ASSETS = [
   "/home.css",
   "/painel.css",
